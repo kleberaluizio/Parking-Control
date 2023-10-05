29359b9ea1fad7434791e1fe33112afddf6f41f8
kleberaluizio <kleberaluizio@gmail.com>
2023-10-05 17:46:25 -0300

    Creating endpoints, adjusting HTTPstatus and understanding the use o BeanUtils.copyProperties()

---
+++ parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,53 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ParkingSpotService {
+
+    private final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public Optional<ParkingSpotModel> getById(UUID id) {
+        Optional<ParkingSpotModel> parkingSpotModel = parkingSpotRepository.findById(id);
+        return parkingSpotModel;
+    }
+
+    public List<ParkingSpotModel> getAll() {
+        return parkingSpotRepository.findAll();
+    }
+
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel) {
+        parkingSpotRepository.delete(parkingSpotModel);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlate) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlate);
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBloc(String apartment, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment,block);
+    }
+
+}
