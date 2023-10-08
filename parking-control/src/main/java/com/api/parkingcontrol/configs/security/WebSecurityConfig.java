9c71b3a6552d171559428fd853ef349921d7db5d
kleberaluizio <kleberaluizio@gmail.com>
2023-10-08 12:01:35 -0300

    Configuring Spring Security

---
+++ parking-control/src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.api.parkingcontrol.configs.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
